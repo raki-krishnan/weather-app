c415477d2bbdf0f053b68864ca0f92f9e46740c6
raki-krishnan <raki@umich.edu>
2024-03-22 20:31:33 -0400

    added CSS

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -10,12 +10,39 @@ import clear_icon from "../Assets/clear.png";
 import wind_icon from "../Assets/wind.png";
 
 const WeatherApp = () => {
+
+    let api_key = "b619c02ef0d0c5ea4a66d9ddf680e09f";
+
+
     return (
         <div className = "container">
             <div className="topbar">
                 <input type="text" className="cityInput" placeholder = "Search"/>
                 <div className = "search-icon">
                     <img src={search_icon} alt="search"/>
+                </div>
+            </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="cloud"/>
+            </div>
+            <div className="weather-temp">
+                24°C
+            </div>
+            <div className="weather-location">London</div>
+            <div className="data-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percentage">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon" />
+                    <div className="data">
+                        <div className="humidity-percentage">18 km/h</div>
+                        <div className="text">Wind Speed</div>
+                    </div>
                 </div>
             </div>
         </div>
