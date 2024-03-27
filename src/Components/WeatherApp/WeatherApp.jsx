a5d43148ab676c093707ad678f8c2bcff2c4025a
raki-krishnan <raki@umich.edu>
2024-03-26 21:33:14 -0400

    Added csv files and added twin button

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './WeatherApp.css';
+import Papa from 'papaparse';
 import search_icon from "../Assets/search.png";
 import cloud_icon from "../Assets/cloud.png";
 import rain_icon from "../Assets/rain.png";
@@ -15,12 +16,14 @@ import nightcloud_icon from "../Assets/nightcloud.png";
 import nightthunderstorm_icon from "../Assets/nightthunderstorm.png";
 import nightsnow_icon from "../Assets/nightsnow.png";
 
+
 const WeatherApp = () => {
 
     const [wicon, setWicon] = useState(cloud_icon);
-    const [error, setError] = useState("");
+    const [cities, setCities] = useState([]);
+    const [error, setError] = useState("");    
     
-    const cities = ["Tokyo", "New York", "London", "Paris", "Sydney", 
+    const citiesList = ["Tokyo", "New York", "London", "Paris", "Sydney", 
     "Moscow", "Cairo", "Rio de Janeiro", "Toronto", "Beijing", "Berlin", "Rome", "Madrid",
     "Dubai", "Seoul", "Istanbul", "Mumbai", "Bangkok", "Los Angeles", "Chicago", "Miami",
     "Las Vegas", "San Francisco", "Washington", "Boston", "Seattle", "Dallas", "Houston",
@@ -33,15 +36,137 @@ const WeatherApp = () => {
     "San Diego", "San Antonio", "San Jose", "New Orleans", "Portland", "Salt Lake City",
     "Barcelona", "Venice", "Milan", "Amsterdam", "Brussels", "Dublin", "Lisbon", "Bucharest",
     "Shanghai", "Hong Kong", "Singapore", "Kuala Lumpur", "Jakarta", "Manila", "Hanoi", 
-    "Normandy", "Taipei", "Auckland", "Wellington", "Christchurch", "Brisbane", "Perth"];
+    "Normandy", "Taipei", "Auckland", "Wellington", "Christchurch", "Brisbane", "Perth",
+    "Adelaide", "Canberra", "Hobart", "Darwin", "Alice Springs", "Cairns", "Gold Coast",
+    "Townsville", "Mackay", "Rockhampton", "Bundaberg", "Hervey Bay", "Fraser Island", 
+    "Luzhou", "Chengdu", "Chongqing", "Wuhan", "Nanjing", "Hangzhou", "Puyang", "Suzhou",
+    "Charlotte", "Indianapolis", "Columbus", "Fort Worth", "El Paso", "Louisville", "Nashville",
+    "Memphis", "Oklahoma City", "Santa Monica", "Baltimore", "Milwaukee", "Albuquerque", "Tucson",
+    "Fresno", "Sacramento", "Long Beach", "Kansas City", "Mesa", "Virginia Beach", "Santa Clara",
+    "Manhattan", "The Bronx", "Buffalo", "Rochester", "Kolkata", "Pune", "Ahmedabad", "Hyderabad",
+    "Lahore", "Karachi", "Islamabad", "Dhaka", "Chittagong", "Colombo", "Kandy", "Galle", "Jaffna",
+    "Munich", "Naples", "Turin", "Palermo", "Genoa", "Bologna", "Florence", "Hamburg", "Bari",
+    "Manchester", "Birmingham", "Leeds", "Glasgow", "Sheffield", "Liverpool", "Bristol", "Cardiff",
+    "Blacksburg", "Durban", "Bloemfontein", "Port Elizabeth", "East London", "Pietermaritzburg",
+    "Jinja", "Kampala", "Tampa", "Mombasa", "Kisumu", "Eldoret", "Nakuru", "Kisii", "Kakamega",
+    "Austin", "Little Rock", "Des Moines", "Boise", "Jackson", "Billings", "Helena", "Cheyenne",
+    "Santa Fe", "Olympia", "Salem", "Carson City", "Juneau", "Fairfax", "Bethesda", "Fairbanks",
+    "Annapolis", "Harrisburg", "Trenton", "Albany", "Hartford", "Providence", "Concord", "Montpelier",
+    "Green Bay", "Madison", "Springfield", "Lansing", "Topeka", "Jefferson City", "Lincoln", "Pierre", 
+    "Ann Arbor", "Boulder", "Bloomington", "Champaign", "Evanston", "Ithaca", "Lawrence", "Lexington"];
+
+
+    const loadCities = async () => {
+        // Assuming your CSV file is located in the `public` directory.
+        const response = await fetch('/simplemaps_worldcities_basicv1.77/worldcities.csv');
+
+        if (!response.ok) {
+            // If the response is not OK, log the error and return early
+            console.error(`HTTP error! status: ${response.status}`);
+            return;
+        }
+
+        // If the response is OK, read the text (CSV content)
+        const csv = await response.text();
+
+        // Now parse the CSV text
+        const results = Papa.parse(csv, { header: true });
+        const rows = results.data; // array of objects
+        setCities(rows);
+        
+        if (rows.length > 0) {
+            console.log("First city from the loaded data:", rows[0]);
+        } else {
+            console.log("No data in CSV");
+        }
+    };
+    
+    useEffect(() => {
+        loadCities();
+        fetchWeatherData("Ann Arbor");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const fetchRandomWeather = () => {
-        const randomIndex = Math.floor(Math.random() * cities.length);
-        const randomCity = cities[randomIndex];
-        fetchWeatherData(randomCity);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        if (cities.length > 0) {
+            const tryFetch = () => {
+                const randomIndex = Math.floor(Math.random() * cities.length);
+                const randomCity = cities[randomIndex];
+    
+                if (!randomCity || randomCity.city === "") {
+                    // Invalid city object, retry
+                    tryFetch();
+                    return;
+                }
+    
+                fetchWeatherData(randomCity.city, () => {
+                    // onSuccess: Log the success or perform other actions
+                    console.log("Weather data fetched successfully for: " + randomCity.city);
+                }, () => {
+                    // onError: Retry with another random city
+                    console.log("Retrying with another city");
+                    tryFetch();
+                });
+            };
+    
+            tryFetch();
+        }
     };
 
+
+
+    let api_key = "b619c02ef0d0c5ea4a66d9ddf680e09f";
+    const element = document.getElementsByClassName("cityInput");
+
+    // const whereElseInTheWorld = async () => {
+    //     const currentCity = document.getElementsByClassName("weather-location")[0].innerHTML;
+    //     const currentCityTemperature = document.getElementsByClassName("weather-temp")[0].innerHTML;;
+    
+    //     // A helper function to delay the loop execution
+    //     const delay = ms => new Promise(res => setTimeout(res, ms));
+    
+    //     // Assuming cities is an array of city objects with a city name property
+    //     const maxAttempts = cities.length;
+    //     const attemptIntervalMs = 10; // Delay between attempts in milliseconds
+    
+    //     for (let i = 0; i < maxAttempts; i++) {
+    //         const randomIndex = Math.floor(Math.random() * cities.length);
+    //         const city = cities[randomIndex].city;
+    
+    //         // Avoid re-checking the current city
+    //         if (city === currentCity) {
+    //             continue;
+    //         }
+    
+    //         try {
+    //             // You will call the fetchWeatherData function with a callback
+    //             const checkTemperatureMatch = (onSuccess, onError) => {
+    //                 fetchWeatherData(city, onSuccess, onError);
+    //             };
+    
+    //             await new Promise((resolve, reject) => {
+    //                 checkTemperatureMatch(() => {
+    //                     // If the temperature matches, we resolve the promise
+    //                     if (currentCityTemperature === currentTemp) {
+    //                         resolve();
+    //                     } else {
+    //                         // Otherwise, we reject and try the next city after a delay
+    //                         setTimeout(() => reject(), attemptIntervalMs);
+    //                     }
+    //                 }, () => {
+    //                     // If the API call fails, we try the next city after a delay
+    //                     setTimeout(() => reject(), attemptIntervalMs);
+    //                 });
+    //             });
+    
+    //             // If we've found a match, we break out of the loop
+    //             break;
+    //         } catch {
+    //             // Continue to the next iteration after a delay
+    //             await delay(attemptIntervalMs);
+    //         }
+    //     }
+    // };
     const whereElseInTheWorld = async () => {
         //We want to show the weather of a different city with the same temperature as the current city
         const temperature = document.getElementsByClassName("weather-temp")[0].innerHTML;
@@ -54,8 +179,8 @@ const WeatherApp = () => {
         const startTime = Date.now(); // Get the current time at the start of the loop
         while (!found) {
             console.log("found before: " + found)
-            randomIndex = Math.floor(Math.random() * cities.length);
-            randomCity = cities[randomIndex];
+            randomIndex = Math.floor(Math.random() * citiesList.length);
+            randomCity = citiesList[randomIndex];
             console.log(randomCity);
             await fetchWeatherData(randomCity);
             console.log("getElembyclass : " + document.getElementsByClassName("weather-temp")[0].innerHTML)
@@ -73,34 +198,32 @@ const WeatherApp = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     };
 
-    let api_key = "b619c02ef0d0c5ea4a66d9ddf680e09f";
-    const element = document.getElementsByClassName("cityInput");
-
-    useEffect(() => {
-        fetchWeatherData("Ann Arbor");
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const fetchWeatherData = async (city) => {
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
+    const fetchWeatherData = async (city, onSuccess, onError) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+    
         try {
-            let response = await fetch(url);
+            const response = await fetch(url);
+            console.log("HOLLAAAAAA " + response);
             if (!response.ok) {
                 throw new Error("City not found");
             }
-            let data = await response.json();
+    
+            const data = await response.json();
+            console.log(data);
+            const cityTemperature = Math.floor((data.main.temp - 273.15) * 9/5 + 32);
+    
+            // Update the UI with the new data
             const humidity = document.getElementsByClassName("humidity-percentage");
             const wind = document.getElementsByClassName("wind-speed");
             const temperature = document.getElementsByClassName("weather-temp");
             const location = document.getElementsByClassName("weather-location");
-
+    
             humidity[0].innerHTML = data.main.humidity + "%";
             wind[0].innerHTML = data.wind.speed + " mph";
-            temperature[0].innerHTML = Math.floor((data.main.temp - 273.15) * 9/5 + 32) + "°F";
-            location[0].innerHTML = data.name;
-            console.log("HELLOOOOO " + data.name)
-
+            temperature[0].innerHTML = cityTemperature + "°F";
+            location[0].innerHTML = data.name + ", " + data.sys.country;
+    
+            // Update weather icon based on the API response
             if(data.weather && data.weather[0].icon) {
                 switch(data.weather[0].icon) {
                     case "01d":
@@ -147,9 +270,16 @@ const WeatherApp = () => {
                         setWicon(cloud_icon);
                 }
             }
+            if (onSuccess) {
+                onSuccess(cityTemperature); 
+            }
         } catch (error) {
+            console.error(error);
             setError("City not found");
             setTimeout(() => setError(""), 3000);
+            if (onError) {
+                onError();
+            }
         }
     };
 
@@ -186,7 +316,7 @@ const WeatherApp = () => {
                 <div className="weather-details-button-container">
                     <div className="weather-details">
                         <div className="weather-temp">57°F</div>
-                        <div className="weather-location">Ann Arbor</div>
+                        <div className="weather-location">Ann Arbor, United States</div>
                     </div>
                     <button className="where-else" onClick={whereElseInTheWorld}>Twin</button>
                 </div>
