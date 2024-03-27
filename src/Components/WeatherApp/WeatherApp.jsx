41abe72f7929f0ab90ca8a2e695b7bff824b8944
raki-krishnan <raki@umich.edu>
2024-03-26 22:05:22 -0400

    added country codes

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -53,9 +53,278 @@ const WeatherApp = () => {
     "Santa Fe", "Olympia", "Salem", "Carson City", "Juneau", "Fairfax", "Bethesda", "Fairbanks",
     "Annapolis", "Harrisburg", "Trenton", "Albany", "Hartford", "Providence", "Concord", "Montpelier",
     "Green Bay", "Madison", "Springfield", "Lansing", "Topeka", "Jefferson City", "Lincoln", "Pierre", 
-    "Ann Arbor", "Boulder", "Bloomington", "Champaign", "Evanston", "Ithaca", "Lawrence", "Lexington"];
+    "Ann Arbor", "Boulder", "Bloomington", "Champaign", "Evanston", "Ithaca", "Lawrence", "Lexington",
+    "Morgantown", "Norman", "Oxford", "Palo Alto", "Princeton", "Raleigh", "State College", "Tallahassee",
+    "New Mexico", "Urbana", "Anaheim", "Berkeley", "Cambridge", "Chapel Hill", "Columbia", "Davis", 
+    "Gainesville", "Montreal", "Quebec City", "Vancouver", "Calgary", "Edmonton", "Ottawa", "Winnipeg",
+    "Halifax", "St. John's", "Regina", "Saskatoon", "Victoria", "Fredericton", "Charlottetown", "Whitehorse",
+    "Farmington Hills", "West Bloomfield", "Troy", "Novi", "Royal Oak", "Birmingham", "Southfield", "Livonia",];
 
 
+
+    const countryCodes = {
+        'AD': 'Andorra',
+        'AE': 'United Arab Emirates',
+        'AF': 'Afghanistan',
+        'AG': 'Antigua & Barbuda',
+        'AI': 'Anguilla',
+        'AL': 'Albania',
+        'AM': 'Armenia',
+        'AN': 'Netherlands Antilles',
+        'AO': 'Angola',
+        'AQ': 'Antarctica',
+        'AR': 'Argentina',
+        'AS': 'American Samoa',
+        'AT': 'Austria',
+        'AU': 'Australia',
+        'AW': 'Aruba',
+        'AZ': 'Azerbaijan',
+        'BA': 'Bosnia and Herzegovina',
+        'BB': 'Barbados',
+        'BD': 'Bangladesh',
+        'BE': 'Belgium',
+        'BF': 'Burkina Faso',
+        'BG': 'Bulgaria',
+        'BH': 'Bahrain',
+        'BI': 'Burundi',
+        'BJ': 'Benin',
+        'BM': 'Bermuda',
+        'BN': 'Brunei Darussalam',
+        'BO': 'Bolivia',
+        'BR': 'Brazil',
+        'BS': 'Bahama',
+        'BT': 'Bhutan',
+        'BU': 'Burma (no longer exists)',
+        'BV': 'Bouvet Island',
+        'BW': 'Botswana',
+        'BY': 'Belarus',
+        'BZ': 'Belize',
+        'CA': 'Canada',
+        'CC': 'Cocos (Keeling) Islands',
+        'CF': 'Central African Republic',
+        'CG': 'Congo',
+        'CH': 'Switzerland',
+        'CI': 'Côte D\'ivoire (Ivory Coast)',
+        'CK': 'Cook Iislands',
+        'CL': 'Chile',
+        'CM': 'Cameroon',
+        'CN': 'China',
+        'CO': 'Colombia',
+        'CR': 'Costa Rica',
+        'CS': 'Czechoslovakia (no longer exists)',
+        'CU': 'Cuba',
+        'CV': 'Cape Verde',
+        'CX': 'Christmas Island',
+        'CY': 'Cyprus',
+        'CZ': 'Czech Republic',
+        'DD': 'German Democratic Republic (no longer exists)',
+        'DE': 'Germany',
+        'DJ': 'Djibouti',
+        'DK': 'Denmark',
+        'DM': 'Dominica',
+        'DO': 'Dominican Republic',
+        'DZ': 'Algeria',
+        'EC': 'Ecuador',
+        'EE': 'Estonia',
+        'EG': 'Egypt',
+        'EH': 'Western Sahara',
+        'ER': 'Eritrea',
+        'ES': 'Spain',
+        'ET': 'Ethiopia',
+        'FI': 'Finland',
+        'FJ': 'Fiji',
+        'FK': 'Falkland Islands (Malvinas)',
+        'FM': 'Micronesia',
+        'FO': 'Faroe Islands',
+        'FR': 'France',
+        'FX': 'France, Metropolitan',
+        'GA': 'Gabon',
+        'GB': 'United Kingdom (Great Britain)',
+        'GD': 'Grenada',
+        'GE': 'Georgia',
+        'GF': 'French Guiana',
+        'GH': 'Ghana',
+        'GI': 'Gibraltar',
+        'GL': 'Greenland',
+        'GM': 'Gambia',
+        'GN': 'Guinea',
+        'GP': 'Guadeloupe',
+        'GQ': 'Equatorial Guinea',
+        'GR': 'Greece',
+        'GS': 'South Georgia and the South Sandwich Islands',
+        'GT': 'Guatemala',
+        'GU': 'Guam',
+        'GW': 'Guinea-Bissau',
+        'GY': 'Guyana',
+        'HK': 'Hong Kong',
+        'HM': 'Heard & McDonald Islands',
+        'HN': 'Honduras',
+        'HR': 'Croatia',
+        'HT': 'Haiti',
+        'HU': 'Hungary',
+        'ID': 'Indonesia',
+        'IE': 'Ireland',
+        'IL': 'Israel',
+        'IN': 'India',
+        'IO': 'British Indian Ocean Territory',
+        'IQ': 'Iraq',
+        'IR': 'Islamic Republic of Iran',
+        'IS': 'Iceland',
+        'IT': 'Italy',
+        'JM': 'Jamaica',
+        'JO': 'Jordan',
+        'JP': 'Japan',
+        'KE': 'Kenya',
+        'KG': 'Kyrgyzstan',
+        'KH': 'Cambodia',
+        'KI': 'Kiribati',
+        'KM': 'Comoros',
+        'KN': 'St. Kitts and Nevis',
+        'KP': 'Korea, Democratic People\'s Republic of',
+        'KR': 'Korea, Republic of',
+        'KW': 'Kuwait',
+        'KY': 'Cayman Islands',
+        'KZ': 'Kazakhstan',
+        'LA': 'Lao People\'s Democratic Republic',
+        'LB': 'Lebanon',
+        'LC': 'Saint Lucia',
+        'LI': 'Liechtenstein',
+        'LK': 'Sri Lanka',
+        'LR': 'Liberia',
+        'LS': 'Lesotho',
+        'LT': 'Lithuania',
+        'LU': 'Luxembourg',
+        'LV': 'Latvia',
+        'LY': 'Libyan Arab Jamahiriya',
+        'MA': 'Morocco',
+        'MC': 'Monaco',
+        'MD': 'Moldova, Republic of',
+        'MG': 'Madagascar',
+        'MH': 'Marshall Islands',
+        'ML': 'Mali',
+        'MN': 'Mongolia',
+        'MM': 'Myanmar',
+        'MO': 'Macau',
+        'MP': 'Northern Mariana Islands',
+        'MQ': 'Martinique',
+        'MR': 'Mauritania',
+        'MS': 'Monserrat',
+        'MT': 'Malta',
+        'MU': 'Mauritius',
+        'MV': 'Maldives',
+        'MW': 'Malawi',
+        'MX': 'Mexico',
+        'MY': 'Malaysia',
+        'MZ': 'Mozambique',
+        'NA': 'Namibia',
+        'NC': 'New Caledonia',
+        'NE': 'Niger',
+        'NF': 'Norfolk Island',
+        'NG': 'Nigeria',
+        'NI': 'Nicaragua',
+        'NL': 'Netherlands',
+        'NO': 'Norway',
+        'NP': 'Nepal',
+        'NR': 'Nauru',
+        'NT': 'Neutral Zone (no longer exists)',
+        'NU': 'Niue',
+        'NZ': 'New Zealand',
+        'OM': 'Oman',
+        'PA': 'Panama',
+        'PE': 'Peru',
+        'PF': 'French Polynesia',
+        'PG': 'Papua New Guinea',
+        'PH': 'Philippines',
+        'PK': 'Pakistan',
+        'PL': 'Poland',
+        'PM': 'St. Pierre & Miquelon',
+        'PN': 'Pitcairn',
+        'PR': 'Puerto Rico',
+        'PT': 'Portugal',
+        'PW': 'Palau',
+        'PY': 'Paraguay',
+        'QA': 'Qatar',
+        'RE': 'Réunion',
+        'RO': 'Romania',
+        'RU': 'Russian Federation',
+        'RW': 'Rwanda',
+        'SA': 'Saudi Arabia',
+        'SB': 'Solomon Islands',
+        'SC': 'Seychelles',
+        'SD': 'Sudan',
+        'SE': 'Sweden',
+        'SG': 'Singapore',
+        'SH': 'St. Helena',
+        'SI': 'Slovenia',
+        'SJ': 'Svalbard & Jan Mayen Islands',
+        'SK': 'Slovakia',
+        'SL': 'Sierra Leone',
+        'SM': 'San Marino',
+        'SN': 'Senegal',
+        'SO': 'Somalia',
+        'SR': 'Suriname',
+        'ST': 'Sao Tome & Principe',
+        'SU': 'Union of Soviet Socialist Republics (no longer exists)',
+        'SV': 'El Salvador',
+        'SY': 'Syrian Arab Republic',
+        'SZ': 'Swaziland',
+        'TC': 'Turks & Caicos Islands',
+        'TD': 'Chad',
+        'TF': 'French Southern Territories',
+        'TG': 'Togo',
+        'TH': 'Thailand',
+        'TJ': 'Tajikistan',
+        'TK': 'Tokelau',
+        'TM': 'Turkmenistan',
+        'TN': 'Tunisia',
+        'TO': 'Tonga',
+        'TP': 'East Timor',
+        'TR': 'Turkey',
+        'TT': 'Trinidad & Tobago',
+        'TV': 'Tuvalu',
+        'TW': 'Taiwan, Province of China',
+        'TZ': 'Tanzania, United Republic of',
+        'UA': 'Ukraine',
+        'UG': 'Uganda',
+        'UM': 'United States Minor Outlying Islands',
+        'US': 'USA',
+        'UY': 'Uruguay',
+        'UZ': 'Uzbekistan',
+        'VA': 'Vatican City State (Holy See)',
+        'VC': 'St. Vincent & the Grenadines',
+        'VE': 'Venezuela',
+        'VG': 'British Virgin Islands',
+        'VI': 'Virgin Islands',
+        'VN': 'Viet Nam',
+        'VU': 'Vanuatu',
+        'WF': 'Wallis & Futuna Islands',
+        'WS': 'Samoa',
+        'YD': 'Democratic Yemen (no longer exists)',
+        'YE': 'Yemen',
+        'YT': 'Mayotte',
+        'YU': 'Yugoslavia',
+        'ZA': 'South Africa',
+        'ZM': 'Zambia',
+        'ZR': 'Zaire',
+        'ZW': 'Zimbabwe',
+        'ZZ': 'Unknown or unspecified country',
+    }
+
+    
+    function adjustTextSize(id, initialFontSize, step) {
+        const element = document.getElementById(id);
+        let currentSize = initialFontSize;
+      
+        // Set the initial font size
+        element.style.fontSize = `${currentSize}px`;
+      
+        // Reduce the font size until the text fits or reaches a minimum size
+        while (element.scrollWidth > element.offsetWidth && currentSize > 12) { // minimum size = 12px
+          currentSize -= step;
+          element.style.fontSize = `${currentSize}px`;
+        }
+    }
+    
     const loadCities = async () => {
         // Assuming your CSV file is located in the `public` directory.
         const response = await fetch('/simplemaps_worldcities_basicv1.77/worldcities.csv');
@@ -88,30 +357,39 @@ const WeatherApp = () => {
     }, []);
 
     const fetchRandomWeather = () => {
-        if (cities.length > 0) {
-            const tryFetch = () => {
-                const randomIndex = Math.floor(Math.random() * cities.length);
-                const randomCity = cities[randomIndex];
+        let retryCount = 0;
+        const maxRetries = 10; // Set a maximum number of retries to prevent infinite loops
     
-                if (!randomCity || randomCity.city === "") {
-                    // Invalid city object, retry
-                    tryFetch();
-                    return;
-                }
+        const tryFetch = () => {
+            if (retryCount >= maxRetries) {
+                console.log("Maximum retries reached, stopping the fetch attempts.");
+                return;
+            }
     
-                fetchWeatherData(randomCity.city, () => {
-                    // onSuccess: Log the success or perform other actions
-                    console.log("Weather data fetched successfully for: " + randomCity.city);
-                }, () => {
-                    // onError: Retry with another random city
-                    console.log("Retrying with another city");
-                    tryFetch();
-                });
-            };
+            const randomIndex = Math.floor(Math.random() * cities.length);
+            const randomCity = cities[randomIndex];
     
-            tryFetch();
-        }
+            if (!randomCity || randomCity.city === "") {
+                // Invalid city object, increment retry count and retry
+                retryCount++;
+                tryFetch();
+                return;
+            }
+    
+            fetchWeatherData(randomCity.city, () => {
+                // onSuccess: Log the success or perform other actions
+                console.log("Weather data fetched successfully for: " + randomCity.city);
+            }, () => {
+                // onError: Log the error, increment retry count, and retry with another random city
+                console.log("Failed to fetch data for city: " + randomCity.city + ", retrying with another city");
+                retryCount++;
+                tryFetch();
+            });
+        };
+    
+        tryFetch();
     };
+    
 
 
 
@@ -221,7 +499,8 @@ const WeatherApp = () => {
             humidity[0].innerHTML = data.main.humidity + "%";
             wind[0].innerHTML = data.wind.speed + " mph";
             temperature[0].innerHTML = cityTemperature + "°F";
-            location[0].innerHTML = data.name + ", " + data.sys.country;
+            location[0].innerHTML = data.name + ", " + countryCodes[data.sys.country];
+            adjustTextSize('weather-location', 50, 1); // Initial size = 60, step = 1
     
             // Update weather icon based on the API response
             if(data.weather && data.weather[0].icon) {
